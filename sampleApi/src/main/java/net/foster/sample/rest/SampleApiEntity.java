e302863527beb419071a2c3b5ffe21269ca1c528
Daryl Foster <daryl.foster@oncenter.com>
2017-12-22 11:24:08 -0600

    First commit

---
+++ sampleApi/src/main/java/net/foster/sample/rest/SampleApiEntity.java
@@ -0,0 +1,181 @@
+/**
+ * 
+ * Copyright (c) 2013 On Center Software, Inc - All Rights Reserved
+ *
+ * SampleApiEntity.java 
+ * 
+ */
+ 
+
+
+package net.foster.sample.rest;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.annotations.Type;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+//~--- classes ----------------------------------------------------------------
+
+/**
+ * Sample Entity
+ *
+ *
+ * @author         Daryl Foster    
+ */
+@ApiModel (
+    value       = "SampleApiEntity",
+    description = "This is a sample entity from the Api package."
+)
+@Entity
+public class SampleApiEntity
+        implements Serializable
+{
+    //~--- constructors -------------------------------------------------------
+
+    /**
+     * Constructs ...
+     *
+     */
+    public SampleApiEntity () {}
+
+    //~--- static fields ------------------------------------------------------
+
+    private static final long serialVersionUID = 1L;
+
+    //~--- fields -------------------------------------------------------------
+
+    @Column (nullable = false)
+    @ApiModelProperty (
+        value    = "level",
+        required = true
+    )
+    private Integer   level;
+    @Column (length = 255)
+    @ApiModelProperty (
+        value    = "description",
+        required = false
+    )
+    private String    description;
+    @Column (nullable = false)
+    @ApiModelProperty (
+        value    = "time",
+        required = true
+    )
+    private Timestamp time;
+    @Id
+    @Column (
+        unique   = true,
+        nullable = false
+    )
+    @Type (type = "pg-uuid")
+    @ApiModelProperty (
+        value    = "id",
+        readOnly = true,
+        dataType = "uuid",
+        example  = "123e4567-e89b-12d3-a456-426655440000"
+    )
+    private UUID      sampleApiEntityId;
+
+    //~--- get methods --------------------------------------------------------
+
+    /**
+     * Method description
+     *
+     *
+     * @return
+     */
+    public String getDescription ()
+    {
+        return this.description;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @return
+     */
+    public Integer getLevel ()
+    {
+        return this.level;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @return
+     */
+    public UUID getSampleApiEntityId ()
+    {
+        return this.sampleApiEntityId;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @return
+     */
+    public Timestamp getTime ()
+    {
+        return this.time;
+    }
+
+    //~--- set methods --------------------------------------------------------
+
+    /**
+     * Method description
+     *
+     *
+     * @param description
+     */
+    public void setDescription (String description)
+    {
+        this.description = description;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param level
+     */
+    public void setLevel (Integer level)
+    {
+        this.level = level;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param sampleApiEntityId
+     */
+    public void setSampleApiEntityId (UUID sampleApiEntityId)
+    {
+        this.sampleApiEntityId = sampleApiEntityId;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param time
+     */
+    public void setTime (Timestamp time)
+    {
+        this.time = time;
+    }
+}
---
+++ sampleApi/src/main/java/net/foster/sample/rest/SampleResource.java
@@ -0,0 +1,151 @@
+/**
+ * 
+ * Copyright (c) 2013 On Center Software, Inc - All Rights Reserved
+ *
+ * SampleResource.java 
+ * 
+ */
+ 
+
+
+package net.foster.sample.rest;
+
+//~--- non-JDK imports --------------------------------------------------------
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import net.foster.sample.model.SampleModelEntity;
+
+//~--- JDK imports ------------------------------------------------------------
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+
+//~--- classes ----------------------------------------------------------------
+
+/**
+ * Sample REST Endpoints
+ *
+ *
+ * @author         Daryl Foster    
+ */
+@Api (tags = {"samples"})
+@Path ("/samples")
+@Stateless
+@TransactionManagement (TransactionManagementType.BEAN)
+public class SampleResource
+{
+    //~--- constructors -------------------------------------------------------
+
+    /**
+     * Constructs ...
+     *
+     */
+    public SampleResource () {}
+
+    //~--- methods ------------------------------------------------------------
+
+    /**
+     * Method description
+     *
+     *
+     * @param sampleParameter
+     * @param request
+     *
+     * @return
+     */
+    @ApiOperation (value = "Retrieve Sample Api Entities")
+    @ApiResponses (value = {@ApiResponse (
+        code              = 200,
+        message           = "Sample Api Entities retrieved",
+        response          = SampleApiEntity.class,
+        responseContainer = "List"
+    ) })
+    @GET
+    @Path ("/api")
+    @Produces (MediaType.APPLICATION_JSON)
+    public Response returnApiSamples (@ApiParam (value = "sampleParameter")
+    @PathParam ("sampleParameter") String sampleParameter, @Context Request request)
+    {
+        SampleApiEntity sample1 = new SampleApiEntity ();
+
+        sample1.setDescription ("This is sample api entity one.");
+        sample1.setLevel (1);
+        sample1.setSampleApiEntityId (UUID.randomUUID ());
+        sample1.setTime (new Timestamp (System.currentTimeMillis ()));
+
+        SampleApiEntity sample2 = new SampleApiEntity ();
+
+        sample2.setDescription ("This is sample api entity two.");
+        sample2.setLevel (2);
+        sample2.setSampleApiEntityId (UUID.randomUUID ());
+        sample2.setTime (new Timestamp (System.currentTimeMillis ()));
+
+        List<SampleApiEntity> samples = new ArrayList<> ();
+
+        samples.add (sample1);
+        samples.add (sample2);
+
+        return Response.ok (samples).build ();
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param sampleParameter
+     * @param request
+     *
+     * @return
+     */
+    @ApiOperation (value = "Retrieve Sample Model Entities")
+    @ApiResponses (value = {@ApiResponse (
+        code              = 200,
+        message           = "Sample Model Entities retrieved",
+        response          = SampleModelEntity.class,
+        responseContainer = "List"
+    ) })
+    @GET
+    @Path ("/model")
+    @Produces (MediaType.APPLICATION_JSON)
+    public Response returnModelSamples (@ApiParam (value = "sampleParameter")
+    @PathParam ("sampleParameter") String sampleParameter, @Context Request request)
+    {
+        SampleModelEntity sample1 = new SampleModelEntity ();
+
+        sample1.setDescription ("This is sample model entity one.");
+        sample1.setLevel (1);
+        sample1.setSampleModelEntityId (UUID.randomUUID ());
+        sample1.setTime (new Timestamp (System.currentTimeMillis ()));
+
+        SampleModelEntity sample2 = new SampleModelEntity ();
+
+        sample2.setDescription ("This is sample model entity two.");
+        sample2.setLevel (2);
+        sample2.setSampleModelEntityId (UUID.randomUUID ());
+        sample2.setTime (new Timestamp (System.currentTimeMillis ()));
+
+        List<SampleModelEntity> samples = new ArrayList<> ();
+
+        samples.add (sample1);
+        samples.add (sample2);
+
+        return Response.ok (samples).build ();
+    }
+}
